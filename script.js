900b9dc758dfe27ca4ea564d208fa8f7f728f9c5
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2026-01-21 17:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function validEmail(str) {
   //your JS code here.
+	if (!str) return false;
+
+  const emailRegex = /^[A-Za-z0-9]+([.-]?[A-Za-z0-9]+)*@[A-Za-z0-9]+([.-]?[A-Za-z0-9]+)*(\.[A-Za-z]{2,3})+$/;
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+ const str = prompt("Enter an email address."); 
 alert(validEmail(str));
